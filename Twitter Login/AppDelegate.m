34f01d4a89944df4dd4e07262d902dfea34a11f1
Teddy Wyly <wylynout@gmail.com>
2015-01-15 22:24:37 -0500

    documentation added to app delegate

--- Twitter Login/AppDelegate.m
+++ Twitter Login/AppDelegate.m
@@ -7,8 +7,20 @@
 //
 
 
-// For Extra, Use in app
+// STEPS TO LAUNCH
 
+// 1. Copy your TWITTER_CONSUMER_KEY and TWITTER_CONSUMER_SECRET keys from twitter.dev into their respective spots in Info.plist
+// 2. Drag in Both Folders located in the External Code folder into your project
+// 3. Add Social.Framework and Accounts.Framework in your build phases tab
+// 3. Go to the TJWTwitterAccountsTableViewController Class
+// 4. Reference the getAccountsBarButtonItemPressed: method to login through twitter (though Social.Framework)
+// 5. Reference the tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath to retrieve both your key and secret key from reverse OAuth.  You can use this information later on the server side to make authenticated calls.  Notice that once you have these paramters, you can use them to get ANY users' followers, not just the followers of the user making the call.
+// 6. Reference the getFollowersBarButtonItemPressed: method to pull followers.  Notice the cursor paramter allowing you to paginate. Each request will give you a new cursor number to pass in for the next call.  When the cursor hits 0, it means you have successfully hit all followers/friends.
+// 7.If at any point you run out of requests, you can use your app to make authenticated requets.  See the below info:
+
+
+
+// This is more easily seen https://dev.twitter.com/oauth/application-only at the example near the bottom
 // Post To
 // https://api.twitter.com/oauth2/token with Authorization Header of the BEARER_CREDENTIALS passed through the base64Encoded Metheod below
 
